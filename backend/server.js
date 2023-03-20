0eb52a889f2bd2a91e3a88bae5bf1a6a9e7da332
Jait <jaitlimaye@gmail.com>
2023-03-19 19:56:54 -0700

    more changes for heroku

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-if(process.env.NODE_ENV === "production")
-{
-    app.use(express.static(path.join(__dirname,"../frontend/build")));
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/build'));
-    })
-}
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    });
+  }
 app.use("/api/v1/restaurants",router);
 app.use("*",(req,res) => res.status(404).json({error: "not found"}));
 
